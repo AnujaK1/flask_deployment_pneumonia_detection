ed09a36aa340fdb320a4e9084e0f69cfc5a866af
AnujaK1 <anuja.kothekar7@gmail.com>
2020-09-06 19:55:00 +0530

    Removed unused functions

--- app.py
+++ app.py
@@ -80,95 +80,7 @@ def create_model():
     outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
     model = Model(inputs=[input_img], outputs=[outputs])
     return model
-class generator_single_channel(keras.utils.Sequence):
     
-    def __init__(self, folder, filenames, pneumonia_locations=None, batch_size=64,
-                 image_size=256, shuffle=True, predict=False):
-        self.folder = folder
-        self.filenames = filenames
-        self.pneumonia_locations = pneumonia_locations
-        self.batch_size = batch_size
-        self.image_size = image_size
-        self.shuffle = shuffle
-        self.predict = predict
-        self.on_epoch_end()
-        
-    def __load__(self, filename):
-        # load dicom file as numpy array
-        img = pydicom.dcmread(os.path.join(self.folder, filename)).pixel_array
-        # create empty mask
-        msk = np.zeros(img.shape)
-        # get filename without extension
-        filename = filename.split('.')[0]
-        # if image contains pneumonia
-        is_pneumonia = int(0)
-        if filename in self.pneumonia_locations:
-            # loop through pneumonia
-            is_pneumonia = int(1)
-            for location in self.pneumonia_locations[filename]:
-                # add 1's at the location of the pneumonia
-                x, y, w, h = location
-                msk[y:y+h, x:x+w] = 1
-        # resize both image and mask
-        img = resize(img, (self.image_size, self.image_size), mode='reflect')
-        msk = resize(msk, (self.image_size, self.image_size), mode='reflect')
-        # if augment then horizontal flip half the time
-        # if self.augment and random.random() > 0.5:
-        #     img = np.fliplr(img)
-        #     msk = np.fliplr(msk)
-
-        # add trailing channel dimension
-        img = np.expand_dims(img, axis=-1)
-        msk = np.expand_dims(msk, axis=-1)
-        is_pneumonia = np.array(is_pneumonia)
-
-        return img, msk
-    
-    def __loadpredict__(self, filename):
-        # load dicom file as numpy array
-        img = pydicom.dcmread(os.path.join(self.folder, filename)).pixel_array
-        # resize image
-        img = resize(img, (self.image_size, self.image_size), mode='reflect')
-        # add trailing channel dimension
-        img = np.expand_dims(img, axis=-1)
-        
-        return img
-        
-    def __getitem__(self, index):
-        # select batch
-        filenames = self.filenames[index*self.batch_size:(index+1)*self.batch_size]
-        # predict mode: return images and filenames
-        if self.predict:
-            # load files
-            imgs = [self.__loadpredict__(filename) for filename in filenames]
-            # create numpy batch
-            imgs = np.array(imgs)
-            
-            return imgs,filenames
-        # train mode: return images and masks
-        else:
-            # load files
-            items = [self.__load__(filename) for filename in filenames]
-            # unzip images and masks
-            imgs, msks = zip(*items)
-            # create numpy batch
-            imgs = np.array(imgs)
-            msks = np.array(msks)
-
-            return imgs,msks
-        
-    def on_epoch_end(self):
-        if self.shuffle:
-            random.shuffle(self.filenames)
-        
-    def __len__(self):
-        if self.predict:
-            # return everything
-            return int(np.ceil(len(self.filenames) / self.batch_size))
-        else:
-            # return full batches only
-            return int(len(self.filenames) / self.batch_size)
-
 model = create_model()
 model.load_weights("model.h5")
 uploads_dir = os.path.join(app.instance_path, 'uploads')
@@ -196,7 +108,6 @@ def predict():
     pred = resize(pred, (1024, 1024), mode='reflect')
     comp = pred[:, :, 0] > 0.5
     comp = measure.label(comp)
-    predictionString = ''
     conf=0
     for region in measure.regionprops(comp):
         y, x, y2, x2 = region.bbox
@@ -219,14 +130,16 @@ def predict_api():
     '''
     f = request.files['file']  
     f.save(os.path.join(uploads_dir,f.filename))
+    print("Stored in: ",os.path.join(uploads_dir,f.filename))
     img = pydicom.dcmread(os.path.join(uploads_dir,f.filename)).pixel_array
+    print("Pixel Array Received")
     img = resize(img, (256, 256), mode='reflect')
     img = np.expand_dims(img, axis=-1)
     prediction=model.predict(img)
+    print("Model Array Received")
     pred = resize(pred, (1024, 1024), mode='reflect')
     comp = pred[:, :, 0] > 0.5
     comp = measure.label(comp)
-    predictionString = ''
     conf=0
     for region in measure.regionprops(comp):
         y, x, y2, x2 = region.bbox
